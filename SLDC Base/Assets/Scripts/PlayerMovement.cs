517c48e56fa6914bc1ef5c9835263f896282f887
Luan Munholi <luanmunholi@gmail.com>
2023-09-10 19:52:15 -0300

    [ADD] Initial Scenario & Player Movement

---
+++ SLDC Base/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float rayLength = 1.4f;
+    [SerializeField] float rayOffsetX = 0.5f;
+    [SerializeField] float rayOffsetY = 0.5f;
+    [SerializeField] float rayOffsetZ = 0.5f;
+
+    Vector3 xOffset;
+    Vector3 yOffset;
+    Vector3 zOffset;
+    Vector3 zAxisOriginA;
+    Vector3 zAxisOriginB;
+    Vector3 xAxisOriginA;
+    Vector3 xAxisOriginB;
+
+    Vector3 targetPosition;
+    Vector3 startPosition;
+
+    bool moving;
+
+    void Update()
+    {
+
+        yOffset = transform.position + Vector3.up * rayOffsetY;
+        zOffset = Vector3.forward * rayOffsetZ;
+        xOffset = Vector3.right * rayOffsetX;
+
+        zAxisOriginA = yOffset + xOffset;
+        zAxisOriginB = yOffset - xOffset;
+
+        xAxisOriginA = yOffset + zOffset;
+        xAxisOriginB = yOffset - zOffset;
+
+        // Draw Debug Rays
+        
+        Debug.DrawLine(
+                zAxisOriginA,
+                zAxisOriginA + Vector3.forward * rayLength,
+                Color.red,
+                Time.deltaTime);
+        Debug.DrawLine(
+                zAxisOriginB,
+                zAxisOriginB + Vector3.forward * rayLength,
+                Color.red,
+                Time.deltaTime);
+
+        Debug.DrawLine(
+                zAxisOriginA,
+                zAxisOriginA + Vector3.back * rayLength,
+                Color.red,
+                Time.deltaTime);
+        Debug.DrawLine(
+                zAxisOriginB,
+                zAxisOriginB + Vector3.back * rayLength,
+                Color.red,
+                Time.deltaTime);
+
+        Debug.DrawLine(
+                xAxisOriginA,
+                xAxisOriginA + Vector3.left * rayLength,
+                Color.red,
+                Time.deltaTime);
+        Debug.DrawLine(
+                xAxisOriginB,
+                xAxisOriginB + Vector3.left * rayLength,
+                Color.red,
+                Time.deltaTime);
+
+        Debug.DrawLine(
+                xAxisOriginA,
+                xAxisOriginA + Vector3.right * rayLength,
+                Color.red,
+                Time.deltaTime);
+        Debug.DrawLine(
+                xAxisOriginB,
+                xAxisOriginB + Vector3.right * rayLength,
+                Color.red,
+                Time.deltaTime);
+
+        if (moving) {
+            if (Vector3.Distance(startPosition, transform.position) > 1f){
+                transform.position = targetPosition;
+                moving = false;
+                return;
+            }
+
+            transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (CanMove(Vector3.forward)) {
+                targetPosition = transform.position + Vector3.forward;
+                startPosition = transform.position;
+                moving = true;
+            }
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            if (CanMove(Vector3.back)) {
+                targetPosition = transform.position + Vector3.back;
+                startPosition = transform.position;
+                moving = true;
+            }
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            if (CanMove(Vector3.left)) {
+                targetPosition = transform.position + Vector3.left;
+                startPosition = transform.position;
+                moving = true;
+            }
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            if (CanMove(Vector3.right)) {
+                targetPosition = transform.position + Vector3.right;
+                startPosition = transform.position;
+                moving = true;
+            }
+        }
+
+    }
+
+    bool CanMove(Vector3 direction) {
+        if (Vector3.Equals(Vector3.forward, direction) || Vector3.Equals(Vector3.back, direction)) {
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX, direction, rayLength)) return false;
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX, direction, rayLength)) return false;
+        }
+        else if (Vector3.Equals(Vector3.left, direction) || Vector3.Equals(Vector3.right, direction)) {
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetZ, direction, rayLength)) return false;
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetZ, direction, rayLength)) return false;
+        }
+
+        return true;
+    }
+
+}
