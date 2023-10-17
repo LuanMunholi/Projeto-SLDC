a6f8db74c1f340599c1d4f8901e2ab92ce53f237
Celsohe <30702924+Celsohe@users.noreply.github.com>
2023-10-17 17:34:31 -0300

    [ADD] Script Level Description

---
+++ SLDC Base/Assets/Projeto Base/AssetsCelso/Script/Celso_LevelDescription.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Celso_LevelDescription
+{
+    public static int[] INTPositon =
+    { 
+        0,  1,  2,  3,  4, 
+        5,  6,  7,  8,  9,
+        10, 11, 12, 13, 14, 
+        15, 16, 17, 18, 19, 
+        20, 21, 22, 23, 24,
+    };
+};
+
+
