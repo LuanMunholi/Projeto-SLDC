b8232fb60666720d5d09e07a8a4f7b6c50f34458
Celsohe <30702924+Celsohe@users.noreply.github.com>
2023-10-18 11:05:27 -0300

    [FIX] lvl format and grid positioning

--- SLDC Base/Assets/Projeto Base/AssetsCelso/Script/Celso_LevelInstantiator.cs
+++ SLDC Base/Assets/Projeto Base/AssetsCelso/Script/Celso_LevelInstantiator.cs
@@ -5,20 +5,40 @@ using UnityEngine.Serialization;
 
 public class Celso_LevelInstantiator : MonoBehaviour
 {
-    [SerializeField] public GameObject edgeBoard; 
-    [SerializeField] private  GameObject airplane1; 
-    [SerializeField] private  GameObject airplane2;
-    private List<int> getLevel = new List<int>();
-    
+    [SerializeField] public GameObject edgeBoard;
+    [SerializeField] private static int _lvlScale = 5;
+    [SerializeField] private int[,] _mapReference;
+    private int[] _upperEdge = new int[_lvlScale];
+    private int[] _lowerEdge = new int[_lvlScale];
+    private int[] _leftEdge = new int[_lvlScale];
+    private int[] _rightEdge = new int[_lvlScale];
+
+
+    private void Awake()
+    {
+        _mapReference = new int[_lvlScale, _lvlScale];
+    }
 
 
     void Start()
     {
-        EdgeInstantiation();
+        EdgeDeterminant();
     }
 
-    private void EdgeInstantiation()
+
+    private void EdgeDeterminant()
     {
-        
+        for (int i = 0; i < _lvlScale; i++)
+        {
+            for (int j = 0; j < _lvlScale; j++)
+            {
+                if (i == 0 || i == _lvlScale - 1 || j == 0 || j == _lvlScale - 1)
+                {
+                    GameObject messer = Instantiate(edgeBoard, this.gameObject.transform, true);
+                    messer.transform.position = new Vector3(i , 1, j * 1f);
+
+                }
+            }
+        }
     }
 }
