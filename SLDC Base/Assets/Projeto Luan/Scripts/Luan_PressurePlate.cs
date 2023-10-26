96055db8fc9679e4d96db2d6774cd8b440fb55e2
Luan Munholi <luanmunholi@gmail.com>
2023-10-26 20:49:06 -0300

    [ADD & UPDATE] Added Pressure Plate Traps + Pyramid Entrance Level Update

---
+++ SLDC Base/Assets/Projeto Luan/Scripts/Luan_PressurePlate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luan_PressurePlate : MonoBehaviour
+{
+
+    public float lowerAmount = 1f;
+    public float transitionDuration = 1f;
+
+    private bool isLowering = false;
+    private Vector3 targetPosition;
+
+    [SerializeField] public GameObject objectToDeactivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isLowering)
+        {
+            isLowering = true;
+            targetPosition = transform.position - new Vector3(0f, lowerAmount, 0f);
+            objectToDeactivate.SetActive(false);
+            StartCoroutine(SmoothLower());
+        }
+    }
+
+    private IEnumerator SmoothLower()
+    {
+        Vector3 initialPosition = transform.position;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitionDuration)
+        {
+            transform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / transitionDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        isLowering = false;
+    }
+
+
+}
